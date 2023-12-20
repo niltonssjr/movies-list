8b3b8f618fdef46b33d3c3f4342011f91682c251
Nilton Junior <niltonssjr@gmail.com>
2023-12-20 18:20:30 -0300

    feat: first movie card version

---
+++ src/components/common/MovieCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-card class="pa-1">
+    <v-sheet>
+      <v-img :src="movie.poster" lazy-src="https://placehold.co/300x500">
+        <template v-slot:placeholder>
+          <v-row class="fill-height ma-0" align="center" justify="center">
+            <v-progress-circular
+              indeterminate
+              color="grey lighten-5"
+            ></v-progress-circular>
+          </v-row>
+        </template>
+      </v-img>
+    </v-sheet>
+    <v-sheet
+      height="100"
+      class="pa-2 d-flex flex-column justify-center align-center"
+    >
+      <div class="text-body-1 text-uppercase font-weight-bold">
+        {{ movie.title }}
+      </div>
+    </v-sheet>
+  </v-card>
+</template>
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import { MovieType } from "@/types/movieType";
+export default Vue.extend({
+  props: {
+    movie: {
+      type: Object as PropType<MovieType>,
+    },
+  },
+});
+</script>
--- src/components/page-components/MoviesList.vue
+++ src/components/page-components/MoviesList.vue
@@ -1,17 +1,28 @@
 <template>
-  <div class="list-wrapper">
-    <ul>
-      <li v-for="(movie, index) in moviesList" :key="index">
-        {{ movie.title }}
-      </li>
-    </ul>
-  </div>
+  <v-sheet>
+    <v-row>
+      <v-col
+        v-for="movie in moviesList"
+        :key="movie.imdbID"
+        xs="12"
+        sm="6"
+        md="4"
+        lg="3"
+      >
+        <movie-card :movie="movie" />
+      </v-col>
+    </v-row>
+  </v-sheet>
 </template>
 <script lang="ts">
 import Vue, { PropType } from "vue";
 import { MovieType } from "@/types/movieType";
+import MovieCard from "../common/MovieCard.vue";
 
 export default Vue.extend({
+  components: {
+    MovieCard,
+  },
   props: {
     moviesList: {
       type: Array as PropType<Array<MovieType>>,
